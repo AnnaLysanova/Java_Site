d08de47554e5fb84dfab1b5657051e69dca7df2e
Анна Лысанова <102955560+AnnaLysanova@users.noreply.github.com>
2025-01-13 11:41:39 +0300

    Созданы новые классы и экземпляры

---
+++ sandbox/src/main/java/Java_Site/sandbox/MyFirstProgram.java
@@ -0,0 +1,25 @@
+package Java_Site.sandbox;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+
+	Square s = new Square(5);
+	System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+	Rectangle r = new Rectangle(4, 6);
+	System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+	}
+
+
+
+	public static double area(Square s) {
+		return s.l * s.l;
+	}
+
+	public static double area(Rectangle r) {
+		return r.a * r.b;
+	}
+
+}
---
+++ sandbox/src/main/java/Java_Site/sandbox/Square.java
@@ -0,0 +1,8 @@
+package Java_Site.sandbox;
+
+public class Square {
+    public double l;
+    public Square(double l) {
+        this.l = l;
+    }
+}
