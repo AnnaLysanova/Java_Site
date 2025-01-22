d26193a95cfb37eee05f2acbcb50d624ec1b810f
Анна Лысанова <102955560+AnnaLysanova@users.noreply.github.com>
2025-01-22 20:27:07 +0300

    Созданы тесты по наполнению двух первых полей заявки и тест по открытию страницы вклада Стратег

--- Site/src/test/java/com/example/Suit1/MenuTests.java
+++ Site/src/test/java/com/example/Suit1/MenuTests.java
@@ -22,20 +22,27 @@ public class MenuTests {
         driver = WebDriverManager.chromedriver().create();
         baseUrl = "https://www.bspb.ru/";
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get(baseUrl);
     }
 
     @Test
-    public void test1() {
+    public void businessZayavkaTest() {
+        driver.get("https://www.bspb.ru/business");
+        driver.findElement(By.id("FIRSTNAME-Имя"));
+        driver.findElement(By.id("FIRSTNAME-Имя")).clear();
+        driver.findElement(By.id("FIRSTNAME-Имя")).sendKeys("Василий");
+        driver.findElement(By.name("PHONENUMBER-Телефон"));
+        driver.findElement(By.name("PHONENUMBER-Телефон")).clear();
+        driver.findElement(By.name("PHONENUMBER-Телефон")).sendKeys("+7 999 999 99 99");
+    }
+
+    @Test
+    public void StrategTest() {
         driver.get(baseUrl);
         driver.findElement(By.xpath("//button[@id='popover-trigger-:R6kqdt9jltmH1:']/p")).click();
         driver.findElement(By.linkText("Стратег")).click();
     }
-    @Test
-    public void businessTest() {
-        driver.get("https://www.bspb.ru/"); 
-        driver.get("https://www.bspb.ru/business");
-        driver.findElement(By.xpath("//*[@id='FIRSTNAME-Имя'")).click();
-    }
+
     @AfterClass(alwaysRun = true)
     public void tearDown() {
         driver.quit();
